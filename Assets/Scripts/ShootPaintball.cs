42bc2e162134ee69f7475d67f2f4b52b7681e9d9
scofflawgames <48799779+scofflawgames@users.noreply.github.com>
2019-09-02 04:26:16 -0400

    Splats with Toy
    Killer!

---
+++ Assets/Scripts/ShootPaintball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPaintball : MonoBehaviour
+{
+    public GameObject projectile;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject paintBall = Instantiate(projectile, this.transform.position, Quaternion.identity) as GameObject;
+            paintBall.GetComponent<Rigidbody>().AddForce(transform.forward * 200);
+        }
+    }
+}
